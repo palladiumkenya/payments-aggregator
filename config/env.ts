baaee52a04b1c30d2722674ac3f63a3e9d9d69f9
Amos Machora <amosmachora13@gmail.com>
2024-07-10 13:21:37 +0300

    refactor: migrated to mysql

--- config/env.ts
+++ config/env.ts
@@ -18,6 +18,31 @@ export const MPESA_APP_BASE_URL = assertValue(
   "Missing environment variable: MPESA_APP_BASE_URL"
 );
 
+export const DATABASE_NAME = assertValue(
+  process.env.DATABASE_NAME,
+  "Missing environment variable: DATABASE_NAME"
+);
+
+export const DATABASE_USER = assertValue(
+  process.env.DATABASE_USER,
+  "Missing environment variable: DATABASE_USER"
+);
+
+export const DATABASE_PASSWORD = assertValue(
+  process.env.DATABASE_PASSWORD,
+  "Missing environment variable: DATABASE_PASSWORD"
+);
+
+export const DATABASE_HOST = assertValue(
+  process.env.DATABASE_HOST,
+  "Missing environment variable: DATABASE_HOST"
+);
+
+export const DB_URL = assertValue(
+  process.env.DB_URL,
+  "Missing environment variable: DB_URL"
+);
+
 export function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage);
