d09e1cf33a649981430e860b9ad85bf6733391bd
Amos Machora <amosmachora13@gmail.com>
2024-08-30 17:24:35 +0300

    refactor: correct data type on confirmation and validation (#12)
    correct data type on confirmation and validation

--- app/api/confirmation/route.ts
+++ app/api/confirmation/route.ts
@@ -1,6 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-  //When this API is hit it does nothing for now. The use case of this API is not currently yet defined but some facilities may register this URL.
-  return NextResponse.json({ message: "confirmed" }, { status: 200 });
+  // When this API is hit it does nothing for now.
+  // The use case of this API is not currently yet defined but some facilities may register this URL.
+  return NextResponse.json({
+    ResultCode: 0,
+    ResultDesc: "Success",
+  });
 };
--- app/api/validation/route.ts
+++ app/api/validation/route.ts
@@ -4,6 +4,6 @@ export const POST = async (req: NextRequest) => {
   //When this API is hit it automatically completes the transaction.
   return NextResponse.json({
     ResultCode: "0",
-    ResultDesc: "Validation request received successfully",
+    ResultDesc: "Accepted",
   });
 };
